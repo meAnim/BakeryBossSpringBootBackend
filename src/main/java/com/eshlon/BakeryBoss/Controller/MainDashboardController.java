394df2c8890cb44a3c0303c7eaee86e73a53a695
FalconZ <animj.emma@gmail.com>
2020-08-01 17:43:56 +0100

    main dashboard controller configured to to rout to dashboard.jsp

--- src/main/java/com/eshlon/BakeryBoss/Controller/MainDashboardController.java
+++ src/main/java/com/eshlon/BakeryBoss/Controller/MainDashboardController.java
@@ -6,5 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainDashboardController {
 	
-	
+	@RequestMapping("/dashboard.user")
+	public String home() {
+		return "views/dashboard.jsp";
+	}
 }
