fecba8a658a4639de36dc1171127c72d044d7683
FalconZ <animj.emma@gmail.com>
2020-08-01 17:23:39 +0100

    MyuserDetails class setup to implement userdetails interface

---
+++ src/main/java/com/eshlon/BakeryBoss/Services/MyUserDetails.java
@@ -0,0 +1,17 @@
+package com.eshlon.BakeryBoss.Services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.eshlon.BakeryBoss.Model.User;
+
+public class MyUserDetails implements UserDetails{
+	
+	
+	
+}
